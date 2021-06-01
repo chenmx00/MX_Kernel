78cce47c3aca9a0c580d5b8133803b3c465297b8
root <root@localhost.localdomain>
2021-06-01 13:41:27 -0700

    added grub multiboot header sepc

--- kernel.asm
+++ kernel.asm
@@ -1,6 +1,11 @@
 ;;kernel.asm
 bits 32  ;;nasm directive - 32 bit
 section .text
+        ;multiboot header spec for grub
+        align 4
+        dd 0x1BADB002 ;magic
+        dd 0x00 ;flags
+        dd - (0x1BADB002 + 0x00) ;checksum
 
 global start
 extern kmain  ;kmain is defined in the c file

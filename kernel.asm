fde8aeb205e6d8c26505b208894c8e7df0a91b82
minxing.chen@certik.org <chenmx0064@gmail.com>
2021-06-06 16:36:50 -0400

    load_port and write_port asm done

--- kernel.asm
+++ kernel.asm
@@ -8,7 +8,27 @@ section .text
         dd - (0x1BADB002 + 0x00) ;checksum
 
 global start
+global keyboard_handler
+global read_port
+global write_port
+global load_init
+
+
 extern kmain  ;kmain is defined in the c file
+extern keyboard_handler ; keyboard handler is definred in the c file for convinience.
+
+read_port:
+  mov edx, [esp + 4] ; Read 4 bytes in memory starting at current stack pointer + 4 bytes (next address, which is the first parameter)
+  in al, dx ;dx is the lower 16 bits for edx and instruction in loads it into al, which is the lower 8 bits of eax.
+  ret
+
+write_port:
+  mov edx, [esp + 4] ; loads the first param(port number) into edx
+  mov al, [esp + 4 + 4] ; loads the second param(actual data) into eax(lower 8 bits)
+  out dx, al ; loads al(actual data) into dx(port number)
+  ret
+
+
 
 start:
   cli  ;block interrupts by calling clear interrupt

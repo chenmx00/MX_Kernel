2c6983e7292c9a9273d4d5a7e53185d97cd21d55
minxing.chen@certik.org <chenmx0064@gmail.com>
2021-06-07 18:52:51 -0400

    experiment with mem

--- kernel.c
+++ kernel.c
@@ -137,11 +137,7 @@ void kmain(void) {
   /** this loop clears the screen(framebuffer)
    * there are 25 lines of 80 columns; each element takes 2bytes */
 
-  while(j < 80 * 25 * 2){
-    vidptr[j] = ' ';
-    vidptr[j+1] = 0x07;
-    j = j + 2;
-  }
+  clear_screen();
 
   j = 0;
   
@@ -149,7 +145,7 @@ void kmain(void) {
    */
   while(str1[j] != '\0'){
     vidptr[i] = str1[j];
-    vidptr[i+1] = 0x00 + (i) % 15 + 1;
+    vidptr[i+1] = 0x07;
     ++j;
     i = i + 2;
   }
@@ -158,7 +154,8 @@ void kmain(void) {
   i = 160;
   while(str2[j] != '\0'){
     vidptr[i] = str2[j];
-    vidptr[i+1] = 0x00 + (i) % 15 + 1;
+    vidptr[i+1] = 0x07;
+    //vidptr[i+1] = 0x00 + (i) % 15 + 1; colorify
     ++j;
     i = i + 2;
   }

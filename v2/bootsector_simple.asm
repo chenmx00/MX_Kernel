7b9bb1a9f341c394f4d7740905d87078e1f22098
minxing.chen@certik.org <chenmx0064@gmail.com>
2021-06-09 23:46:11 -0400

    ss

--- v2/bootsector_simple.asm
+++ v2/bootsector_simple.asm
@@ -1,51 +1,42 @@
-mov ah, 0x0e
+mov ah, 0x0e ; tty mode
 
-; attempt 1
-; Fails because it tries to print the memory address (i.e. pointer)
-; not its actual contents
-mov al, "1"
-int 0x10
-mov al, the_secret
-int 0x10
+mov bp, 0x8000 ; this is an address far away from 0x7c00 so that we don't get overwritten
+mov sp, bp ; if the stack is empty then sp points to bp
 
-; attempt 2
-; It tries to print the memory address of 'the_secret' which is the correct approach.
-; However, BIOS places our bootsector binary at address 0x7c00
-; so we need to add that padding beforehand. We'll do that in attempt 3
-mov al, "2"
-int 0x10
-mov al, [the_secret]
+push 'A'
+push 'B'
+push 'C'
+
+; to show how the stack grows downwards
+mov al, [0x7ffe] ; 0x8000 - 2
 int 0x10
 
-; attempt 3
-; Add the BIOS starting offset 0x7c00 to the memory address of the X
-; and then dereference the contents of that pointer.
-; We need the help of a different register 'bx' because 'mov al, [ax]' is illegal.
-; A register can't be used as source and destination for the same command.
-mov al, "3"
-int 0x10
-mov bx, the_secret
-add bx, 0x7c00
-mov al, [bx]
-int 0x10
-
-; attempt 4
-; We try a shortcut since we know that the X is stored at byte 0x2d in our binary
-; That's smart but ineffective, we don't want to be recounting label offsets
-; every time we change the code
-mov al, "4"
-int 0x10
-mov al, [0x7c2d]
+; however, don't try to access [0x8000] now, because it won't work
+; you can only access the stack top so, at this point, only 0x7ffe (look above)
+mov al, [0x8000]
 int 0x10
 
 
-jmp $ ; infinite loop
+; recover our characters using the standard procedure: 'pop'
+; We can only pop full words so we need an auxiliary register to manipulate
+; the lower byte
+pop bx
+mov al, bl
+int 0x10 ; prints C
 
-the_secret:
-    ; ASCII code 0x58 ('X') is stored just before the zero-padding.
-    ; On this code that is at byte 0x2d (check it out using 'xxd file.bin')
-    db "X"
+pop bx
+mov al, bl
+int 0x10 ; prints B
 
-; zero padding and magic bios number
+pop bx
+mov al, bl
+int 0x10 ; prints A
+
+; data that has been pop'd from the stack is garbage now
+mov al, [0x8000]
+int 0x10
+
+
+jmp $
 times 510-($-$$) db 0
 dw 0xaa55

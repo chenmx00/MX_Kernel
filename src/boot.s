1962dd8e9aa0bce6228e4aa6d8491c590888886a
minxing.chen@certik.org <chenmx0064@gmail.com>
2021-08-08 00:11:58 -0400

    asm

--- src/boot.s
+++ src/boot.s
@@ -15,30 +15,36 @@ global keyboard_handler
 global read_port
 global write_port
 global load_idt
+global gdt_flush
 
 extern kmain  ;kmain is defined in the c file
 extern keyboard_handler_main ; keyboard handler is definred in the c file for convinience.
 
 read_port:
-  mov edx, [esp + 4] ; Read 4 bytes in memory starting at current stack pointer + 4 bytes (next address, which is the first parameter)
+  mov edx, [esp + 4] ;Read 4 bytes in memory starting at current stack pointer + 4 bytes (next address, which is the first parameter)
   in al, dx ;dx is the lower 16 bits for edx and instruction in loads it into al, which is the lower 8 bits of eax.
   ret
 
 write_port:
-  mov edx, [esp + 4] ; loads the first param(port number) into edx
-  mov al, [esp + 4 + 4] ; loads the second param(actual data) into eax(lower 8 bits)
-  out dx, al ; loads al(actual data) into dx(port number)
+  mov edx, [esp + 4] ;Loads the first param(port number) into edx
+  mov al, [esp + 4 + 4] ;Loads the second param(actual data) into eax(lower 8 bits)
+  out dx, al ;Loads al(actual data) into dx(port number)
   ret
 
 load_idt:
-  mov edx, [esp + 4] ; loads the first param (idt_ptr)
-  lidt [edx] ; load idt
-  sti ; turns on interruption
+  mov edx, [esp + 4] ;Loads the first param (idt_ptr)
+  lidt [edx] ;Load idt
+  sti ;Turns on interruption
   ret
 
 keyboard_handler:
   call keyboard_handler_main
   iretd
+
+gdt_flush:
+  mov eax, [esp + 4] ;Get the pointer of GDT, passed as parameter.
+  lgdt [eax] ;Call instruction lgdt to load the GDT pointer.
+  
 
 start:
   cli  ;block interrupts by calling clear interrupt

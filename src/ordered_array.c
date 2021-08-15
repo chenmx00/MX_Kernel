df67a8fca904ef36779d7ca1d62aa7c52a3512ca
minxing.chen@certik.org <chenmx0064@gmail.com>
2021-08-15 00:00:43 -0400

    ordered_array.c

---
+++ src/ordered_array.c
@@ -0,0 +1,13 @@
+//ordered_array.h --Implementation for creating, inserting and deleting from ordered arrays.
+//Ian C.
+#include "ordered_array.h"
+s8int standard_less_than_predicate(type_t a, type_t b){
+    return (a < b)? 1 : 0;
+}
+
+ordered_array_t create_ordered_array(s32int max_size, lessthan_predicate_t lessthan){
+    ordered_array_t ret;
+    ret.array = (void*) kmalloc(max_size * sizeof(type_t));
+    memset(ret.array, 0, max_size * sizeof(type_t));
+    ret.size = 0;
+}

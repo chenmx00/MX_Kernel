40a2a7d40fbe54bc99529b1f9716016b78c45b13
minxing.chen@certik.org <chenmx0064@gmail.com>
2021-08-14 23:48:18 -0400

    ordered_array

--- src/ordered_array.h
+++ src/ordered_array.h
@@ -14,5 +14,12 @@ typedef struct
 
 }ordered_array_t;
 
+s8int standard_less_than_predicate(type_t a, type_t b);
 
+ordered_array_t create_ordered_array(u32int max_size, lessthan_predicate_t less_than);
+ordered_array_t place_ordered_array(void *addr, u32int max_size, lessthan_predicate_t less_than);
+void destroy_ordered_array(ordered_array_t *array);
+void insert_ordered_array(type_t item, ordered_array_t *array);
+type_t lookup_ordered_array(u32int index, ordered_array_t *array);
+void remove_ordered_array(u32int index, ordered_array_t *array);
 #endif

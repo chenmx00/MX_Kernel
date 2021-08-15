a25bb3c5149da008c7ffd1a501081862bf140cf4
minxing.chen@certik.org <chenmx0064@gmail.com>
2021-08-15 12:42:50 -0400

    kheap

--- src/kheap.h
+++ src/kheap.h
@@ -1,8 +1,26 @@
 //kheap.h - Declares interfaces for kernel space allocation.
 //Ian C.
 #include "common.h"
+#define KHEAP_START 0XC0000000
+#define HEAP_INITIAL_SIZE 0X100000
+#define HEAP_INDEX_SIZE 0X20000
+#define HEAP_MAGIC 0X5465255C
+#define HEAP_MIN_SIZE 0X70000
 u32int kmalloc(u32int size);
 u32int kmalloc_aligned(u32int size);
 u32int kmalloc_physical(u32int size, u32int *phys);
 u32int kmalloc_aligned_physical(u32int size, u32int *phys);
+
+typedef struct {
+    u32int magic;
+    u8int is_hole;
+    u32int size;
+}header_t;
+
+typedef struct{
+    u32int magic;
+    header_t *header;
+}footer_t;
+
+
 
--- src/ordered_array.c
+++ src/ordered_array.c
@@ -1,11 +1,12 @@
 //ordered_array.h --Implementation for creating, inserting and deleting from ordered arrays.
 //Ian C.
 #include "ordered_array.h"
+#include "kheap.h"
 s8int standard_less_than_predicate(type_t a, type_t b){
     return (a < b)? 1 : 0;
 }
 
-ordered_array_t create_ordered_array(s32int max_size, lessthan_predicate_t lessthan){
+ordered_array_t create_ordered_array(u32int max_size, lessthan_predicate_t lessthan){
     ordered_array_t ret;
     ret.array = (void*) kmalloc(max_size * sizeof(type_t));
     memset(ret.array, 0, max_size * sizeof(type_t));
@@ -13,4 +14,54 @@ ordered_array_t create_ordered_array(s32int max_size, lessthan_predicate_t lesst
     ret.max_size = max_size;
     ret.lessthan = lessthan;
     return ret;
+}
+
+ordered_array_t place_ordered_array(void *address, u32int max_size, lessthan_predicate_t lessthan){
+    ordered_array_t ret;
+    ret.array = (type_t*) address;
+    memset(ret.array, 0, max_size * sizeof(type_t));
+    ret.size = 0;
+    ret.max_size = max_size;
+    ret.lessthan = lessthan;
+    return ret;
+}
+
+void destroy_ordered_array(ordered_array_t *array){
+    //kfree(array->array);
+}
+
+void insert_ordered_array(type_t item, ordered_array_t *array){
+    if (array->lessthan){
+        u32int iterator = 0;
+        while(iterator < array->size && array->lessthan(array->array[iterator], item)){
+            iterator++;
+        }
+        if (array->size == iterator){
+            array->array[array->size++] = item;
+        } else {
+            type_t tmp = array->array[iterator];
+            array->array[iterator] = item;
+            while (iterator < array->size)
+            {
+                iterator++;
+                type_t tmp2 = array->array[iterator];
+                array->array[iterator] = tmp;
+                tmp = tmp2;
+            }
+            array->size++;
+        }
+    }
+}
+
+type_t lookup_ordered_array(u32int index, ordered_array_t *array){
+    if (index < array->size){
+        return array->array[index];
+    }
+}
+
+void remove_ordered_array(u32int index, ordered_array_t *array){
+    while (index < array->size){
+        array->array[index] = array->array[++index];
+    }
+    array->size--;
 }

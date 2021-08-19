cd9d623cf814475af55efdf2b549d3881456d31e
minxing.chen@certik.org <chenmx0064@gmail.com>
2021-08-19 17:38:13 -0400

    revised

--- src/fs.c
+++ src/fs.c
@@ -6,7 +6,10 @@ fs_node_t* fs_root = 0;
 u32int read_fs(fs_node_t* node, u32int offset, u32int size, u8int* buffer){
     if (node->read != 0)
     {
+        return node->read(node, offset, size, buffer);
+    } else {
         return 0;
     }
-    
 }
+
+if 

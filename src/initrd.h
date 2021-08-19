0c6897b38d6b515505a64526920106cdf3b733d4
minxing.chen@certik.org <chenmx0064@gmail.com>
2021-08-19 19:58:43 -0400

    revised

---
+++ src/initrd.h
@@ -0,0 +1,17 @@
+#ifndef INITRD_H
+#define INITRD_H
+#include "common.h"
+typedef struct
+{
+    u32int file_quantity;
+}initrd_header_t;
+typedef struct 
+{
+    u8int magic;
+    s8int name[64]; //file name
+    u8int offset; // offset of that file in the initrd that the file starts
+    u32int length; // size of that file
+}initrd_file_header_t
+
+fs_node_t* initialize_initrd(u32int location);
+#endif

f8d54271095d7d3243526411f8954387ec677ad3
minxing.chen@certik.org <chenmx0064@gmail.com>
2021-08-19 22:31:13 -0400

    revised

--- src/initrd.c
+++ src/initrd.c
@@ -47,21 +47,44 @@ fs_node_t* initialize_initrd(u32int locaiton){
     initrd_header = (initrd_header_t *) locaiton;
     file_header = (initrd_file_header_t *) (locaiton + sizeof(initrd_header_t));
     initrd_root = (fs_node_t*) kmalloc(sizeof(fs_node_t));
-    strcpy(initrd_root, "/");
-    initrd_root->mask = initrd_root->uid = initrd_root->gid = initrd_root->inode = initrd_root->length = initrd_root->read = initrd_root->write = initrd_root->open = initrd_root->close = initrd_root->ptr =  initrd_root->impl = 0;
+    strcpy(initrd_root->name, "/");
+    initrd_root->mask = initrd_root->uid = initrd_root->gid = initrd_root->inode = initrd_root->length = 0;
+    initrd_root->read = 0;
+    initrd_root->write = 0;
+    initrd_root->open = 0;
+    initrd_root->close = 0;
+    initrd_root->ptr =  0;
+    initrd_root->impl = 0;
     initrd_root->flags = FS_DIRECTORY;
     initrd_root->readdir = &initrd_readdir;
     initrd_root->finddir = &initrd_finddir;
     initrd_dev = (fs_node_t*) kmalloc(sizeof(fs_node_t));
-    strcpy(initrd_dev, "dev");
-    initrd_dev->mask = initrd_dev->uid = initrd_dev->gid = initrd_dev->inode = initrd_dev->length = initrd_dev->read = initrd_dev->write = initrd_dev->open = initrd_dev->close = initrd_dev->ptr =  initrd_dev->impl = 0;
+    strcpy(initrd_dev->name, "dev");
+    initrd_dev->mask = initrd_dev->uid = initrd_dev->gid = initrd_dev->inode = initrd_dev->length = 0;
+    initrd_dev->read = 0;
+    initrd_dev->write = 0;
+    initrd_dev->open = 0;
+    initrd_dev->close = 0;
+    initrd_dev->ptr =  0;
+    initrd_dev->impl = 0;
     initrd_dev->flags = FS_DIRECTORY;
     initrd_dev->readdir = &initrd_readdir;
     initrd_dev->finddir = &initrd_finddir;
     root_nodes = (fs_node_t *) kmalloc(sizeof(fs_node_t) * initrd_header->file_quantity);
     file_quantity = initrd_header->file_quantity;
     for(int i = 0; i < initrd_header->file_quantity; i++){
-        
+        file_header[i].offset += locaiton;
+        strcpy(root_nodes[i].name, (char*)&file_header[i].name);
+        root_nodes[i].mask = root_nodes[i].gid = root_nodes[i].uid = 0;
+        root_nodes[i].inode = i;
+        root_nodes[i].flags = FS_FILE;
+        root_nodes[i].read = &initrd_read;
+        root_nodes[i].write = 0;
+        root_nodes[i].readdir = 0;
+        root_nodes[i].finddir = 0;
+        root_nodes[i].open = 0;
+        root_nodes[i].close = 0;
+        root_nodes[i].impl = 0;
     }
     return initrd_root;
 }

0b7af16ec7b62b6195242cd23a9e04f552792aa2
minxing.chen@certik.org <chenmx0064@gmail.com>
2021-08-20 00:49:19 -0400

    revised

--- src/boot.s
+++ src/boot.s
@@ -1,24 +1,46 @@
-;;kernel.asm
-;;Ian C.
-;;06/06/2021
-bits 32  ;;nasm directive - 32 bit
-section .text
-        ;multiboot header spec for grub
-        align 4
-        dd 0x1BADB002 ;magic
-        dd 0x00 ;flags
-        dd - (0x1BADB002 + 0x00) ;checksum
+;
+; boot.s -- Kernel start location. Also defines multiboot header.
+;           Based on Bran's kernel development tutorial file start.asm
+;
 
-global start
+MBOOT_PAGE_ALIGN    equ 1<<0    ; Load kernel and modules on a page boundary
+MBOOT_MEM_INFO      equ 1<<1    ; Provide your kernel with memory info
+MBOOT_HEADER_MAGIC  equ 0x1BADB002 ; Multiboot Magic value
+; NOTE: We do not use MBOOT_AOUT_KLUDGE. It means that GRUB does not
+; pass us a symbol table.
+MBOOT_HEADER_FLAGS  equ MBOOT_PAGE_ALIGN | MBOOT_MEM_INFO
+MBOOT_CHECKSUM      equ -(MBOOT_HEADER_MAGIC + MBOOT_HEADER_FLAGS)
 
-extern kmain  ;kmain is defined in the c file
+
+[BITS 32]                       ; All instructions should be 32-bit.
+
+[GLOBAL mboot]                  ; Make 'mboot' accessible from C.
+[EXTERN code]                   ; Start of the '.text' section.
+[EXTERN bss]                    ; Start of the .bss section.
+[EXTERN end]                    ; End of the last loadable section.
+
+mboot:
+    dd  MBOOT_HEADER_MAGIC      ; GRUB will search for this value on each
+                                ; 4-byte boundary in your kernel file
+    dd  MBOOT_HEADER_FLAGS      ; How GRUB should load your file / settings
+    dd  MBOOT_CHECKSUM          ; To ensure that the above values are correct
+    
+    dd  mboot                   ; Location of this descriptor
+    dd  code                    ; Start of kernel '.text' (code) section.
+    dd  bss                     ; End of kernel '.data' section.
+    dd  end                     ; End of kernel.
+    dd  start                   ; Kernel entry point (initial EIP).
+
+[GLOBAL start]                  ; Kernel entry point.
+[EXTERN kmain]                   ; This is the entry point of our C code
 
 start:
-  cli  ;block interrupts by calling clear interrupt
-  mov esp,stack_space  ;set esp as stack_space
-  call kmain
-  hlt  ;halt the CPU
+    ; Load multiboot information:
+    push    ebx
 
-section .bss
-resb 8192  ;8kib for stack
-stack_space:
+    ; Execute the kernel:
+    cli                         ; Disable interrupts.
+    call kmain                   ; call our main() function.
+    jmp $                       ; Enter an infinite loop, to stop the processor
+                                ; executing whatever rubbish is in the memory
+                                ; after our kernel!
--- src/kmain.c
+++ src/kmain.c
@@ -3,13 +3,19 @@
  * Ian C.
  * 06/06/2021
  */
+#include "multiboot.h"
 #include "descriptor_tables.h"
 #include "monitor.h"
 #include "keyboard.h"
 #include "timer.h"
 #include "paging.h"
 #include "kheap.h"
-void kmain(void) {
+#include "initrd.h"
+#include "fs.h"
+extern u32int placement_address;
+int initrd_load = 0;
+u32int initrd_location = 0;
+void kmain(struct multiboot *mboot_ptr) {
   monitor_clear();
   monitor_write("Kernel version 0.08\n");
   monitor_write("Minxing's kernel\n");
@@ -20,10 +26,22 @@ void kmain(void) {
   monitor_write("-Initializing Keyboards...\n");
   keyboard_init();
   monitor_write("-Done Initializing\n");
+  if (mboot_ptr->mods_count > 0){
+    initrd_location = *((u32int*)mboot_ptr->mods_addr);
+    u32int initrd_end = *(u32int*)(mboot_ptr->mods_addr+4);
+    placement_address = initrd_end;
+    initrd_load = 1;
+  }
+  
   monitor_write("-Initializing Segmentation and Paging...\n");
   initialize_paging();
   monitor_write("-Done Initializing\n");
   monitor_write("-Ready To Use\r");
+  if(initrd_load){
+    monitor_write("-Initializing Inital Ram Disk...\n");
+    fs_root = initialize_initrd(initrd_location);
+    monitor_write("-Done Initializing\n");
+  }
 
  
 

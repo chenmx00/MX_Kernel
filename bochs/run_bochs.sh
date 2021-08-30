d99d5e75640a5778cd853b34328cdec5e01ed07e
minxing.chen@certik.org <chenmx0064@gmail.com>
2021-08-29 20:51:52 -0400

    revised

--- bochs/run_bochs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# run_bochs.sh
-# mounts the correct loopback device, runs bochs, then unmounts.
-
-sudo /sbin/losetup /dev/loop0 mx_kernel.img
-sudo bochs -f bochsrc.txt
-sudo /sbin/losetup -d /dev/loop0
--- bochs/update_image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo /sbin/losetup /dev/loop0 mx_kernel.img
-sudo mount /dev/loop0 /mnt
-sudo cp ../kernel /mnt/kernel
-sudo cp ../initrd_generator/initrd.img /mnt/initrd
-sudo umount /dev/loop0
-sudo /sbin/losetup -d /dev/loop0
